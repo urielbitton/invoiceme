df6686eb6d4a6b806e27add2c542d0a3633bdc7d
urielbitton <urielas@hotmail.com>
2022-12-13 17:23:27 -0500

    reset password done

--- src/app/auth/handlers/ResetPasswordHandler.js
+++ src/app/auth/handlers/ResetPasswordHandler.js
@@ -8,29 +8,36 @@ import { useNavigate } from "react-router-dom"
 export default function ResetPasswordHandler({oobCode}) {
 
   const [newPassword, setNewPassword] = useState('')
+  const [loading, setLoading] = useState(false)
   const auth = firebase.auth()
   const navigate = useNavigate()
 
   const handleResetPassword = (oobCode) => {
+    if(newPassword.length < 5) return alert('Password must be at least 5 characters long.')
+    setLoading(true)
     auth.verifyPasswordResetCode(oobCode)
     .then((email) => {
-      auth.confirmPasswordReset(oobCode, newPassword)
+      return auth.confirmPasswordReset(oobCode, newPassword)
       .then((res) => {
-        alert('Password reset successful')
-        auth.signInWithEmailAndPassword(email, newPassword)
+        alert('Password reset successful. Signing you in...')
+        return auth.signInWithEmailAndPassword(email, newPassword)
         .then(() => {
           navigate('/')
+          setLoading(false)
         })
         .catch((err) => {
           alert('There was an error signing you in. Please try again.')
           console.log(err)
+          setLoading(false)
         })
       })
       .catch((err) => {
         alert('An error occured, make sure the password is valid and the reset code has not expired.')
         console.log(err)
+        setLoading(false)
       })
     }).catch((err) => {
+      setLoading(false)
       alert('The reset code has expired or is invalid. Please try again.')
       console.log(err)
     })
@@ -41,6 +48,7 @@ export default function ResetPasswordHandler({oobCode}) {
       contentImg={resetPasswordImg}
       title="Reset your password"
       description="Create a new password for your account."
+      loading={loading}
       customComponent={
         <div className="password-section">
           <input 
@@ -52,6 +60,7 @@ export default function ResetPasswordHandler({oobCode}) {
           <AppButton
             label="Reset password"
             onClick={() => handleResetPassword(oobCode)}
+            disabled={loading || !newPassword.length}
           />
         </div>
       }
